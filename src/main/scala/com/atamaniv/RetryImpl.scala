d42b7ce5196de9842f1bdee8020a807c9d867cf9
Andriy Atamaniv <andriy.atamaniv@gmail.com>
2019-04-16 20:56:03 +0300

    Story-2: added files back

---
+++ src/main/scala/com/atamaniv/RetryImpl.scala
@@ -0,0 +1,28 @@
+package com.atamaniv
+
+import scala.annotation.tailrec
+import scala.concurrent.duration.FiniteDuration
+
+class RetryImpl extends Retry {
+
+  @tailrec
+  override final def retry[A](block: () => A, acceptResult: A => Boolean, retries: List[FiniteDuration]): A = {
+    val result = block()
+
+    if (acceptResult(result))
+      result
+    else retries match {
+      case head :: Nil =>
+        sleepFor(head.toMillis)
+        retry(block, acceptResult, Nil)
+      case head :: tail =>
+        sleepFor(head.toMillis)
+        retry(block, acceptResult, tail)
+      case Nil => throw new Throwable("Cannot retry any more")
+    }
+  }
+
+  private def sleepFor(mills: Long): Unit = {
+    Thread.sleep(mills)
+  }
+}
