0a4d1a2a3a3aa1af98e80fa4af703113285cd193
Andriy Atamaniv <andriy.atamaniv@gmail.com>
2019-04-17 21:28:32 +0300

    Inspection Comments

--- src/main/scala/com/atamaniv/Retry.scala
+++
@@ -1,7 +0,0 @@
-package com.atamaniv
-
-import scala.concurrent.duration.FiniteDuration
-
-trait Retry {
-  def retry[A](block: () => A, acceptResult: A => Boolean, retries: List[FiniteDuration]): A
-}
